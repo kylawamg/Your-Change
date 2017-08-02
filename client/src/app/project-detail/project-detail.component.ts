7337111eb6041f1fd0cf923900e3e1bc806e8d47
alejandro <alejandro.m.gomez.92@gmail.com>
2017-08-02 16:05:49 +0200

    create-component view done

--- client/src/app/project-detail/project-detail.component.ts
+++ client/src/app/project-detail/project-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ProjectService } from '../services/project.service';
 @Component({
   selector: 'app-project-detail',
   templateUrl: './project-detail.component.html',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectDetailComponent implements OnInit {
 
-  constructor() { }
+  project: any;
+  error:string;
+  constructor(private router: Router, private projectSvc: ProjectService) { }
 
   ngOnInit() {
   }
 
+  getProjectDetail(id){
+    this.projectSvc.getProjectDetail(id)
+      .subscribe(
+        (project) => this.successCb(project),
+        (err) => this.errorCb(err)
+    );
+  }
+  errorCb(err) {
+    this.error = err;
+    console.log(this.error)
+    this.project = null;
+  }
+
+  successCb(project) {
+    this.project = project;
+    this.error = null;
+    this.router.navigate(['/'])
+  }
 }
